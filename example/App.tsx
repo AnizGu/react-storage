d2745913c96963a272bee784e21719338c83190a
AnizGu <gujiwen2020@163.com>
2023-02-13 04:55:22 +0800

    更新版本

--- example/App.tsx
+++ example/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { ReactStorage } from '../src/index';
